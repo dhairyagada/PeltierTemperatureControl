65c08973ba8342fb44c44a503e69ce417a4b4142
Dhairya <dhairyagada1997@gmail.com>
2018-04-20 13:37:01 +0530

    Updated PID

--- Testing_PotInput.ino
+++ Testing_PotInput.ino
@@ -17,7 +17,20 @@ int sensorPin=A0;
 int tempPin = A4;
 int val=0;
 
-int kp=50,kd=18,ki=1;
+// PID Values for Cooling
+int kp=20,kd=12,ki=1;
+
+// PID Values for Heating 
+//int kp=30,kd=580,ki=0.05;
+
+////////////////////////////////////////////
+// PID Values for Cooling
+//int kp=20,kd=12,ki=1;
+
+//kp=10,kd=500,ki=0; -- for 49 at 46
+//kp=20,kd=550,ki=0; -- for 49 at 47-48
+//kp=30,kd=580,ki=0; -- for 49 at 48-49-50
+////////////////////////////////////////////
 
 int temperature_read=0;
 int set_temperature=0;
@@ -32,7 +45,8 @@ void setup()
 {
             Time = millis(); 
             // Timer Setup for High Frequency PWM
-           
+           pinMode(10,OUTPUT);
+           pinMode(9,OUTPUT);
             pinMode(3, OUTPUT);
             //pinMode(11,OUTPUT);
             TCCR2A = _BV(COM2A1) | _BV(COM2B1) | _BV(WGM20);  
@@ -63,7 +77,7 @@ void loop()
   temp=analogRead(sensorPin);
   
   potchange=temp;
-  temp1=-10+(temp/1023.0)*50.0;
+  temp1=15+(temp/1023.0)*41.0;
 
   // LCD Printing
   
@@ -73,6 +87,7 @@ void loop()
   lcd.setCursor(0,1);
   lcd.print("Actual Temp:");
   lcd.print((int)DHT.temperature);
+  //lcd.print(cel);
   lcd.setCursor(15,1);
   lcd.print("C");
 
@@ -82,19 +97,20 @@ void loop()
   
 //  Serial.println(" LM 35 Temp  ");
 //  Serial.println((int)(cel));
-  Serial.println(" Actual Temperature  ");
+//  Serial.println(" Actual Temperature  ");
   Serial.println(DHT.temperature);  
-  Serial.println(" Set Temperature ");
+//  Serial.println(" Set Temperature ");
   Serial.println(temp1);
   
   set_temperature=temp1;
   temperature_read= DHT.temperature;
-  PID_error = abs(set_temperature - temperature_read) ;
-  Serial.println("PID Error");
-  Serial.println(PID_error);
+  //temperature_read= cel;
+  PID_error = (temperature_read - set_temperature) ;
+//  Serial.println("PID Error");
+//  Serial.println(PID_error);
   PID_p = kp * PID_error;
-  
-  PID_i = 0.01*PID_i + (ki * PID_error);
+ 
+  PID_i = (PID_i + (ki * PID_error));
   
   timePrev = Time;                            
   Time = millis();                            
@@ -103,16 +119,33 @@ void loop()
 
   PID_value = PID_p + PID_d + PID_i ;
 
-   if(PID_value < 0)
-    {    PID_value = 0;    }
+   if(PID_value < 0 || PID_error<0 )
+    {    PID_value = 0;
+        //|| PID_p < 0 || PID_error<0
+//         Serial.println("Flag");  
+    }
   if(PID_value > 255)  
     {    PID_value = 255;  }
   
   OCR2B=PID_value;
   previous_error = PID_error; 
   
-  Serial.println("PWM Value ");
-  Serial.println(PID_value);
+//  Serial.println("PWM Value ");
+//  Serial.println(PID_value);
+//  Serial.println("PID P");
+//  Serial.println(PID_p);
+//  Serial.println("PID D");
+//  Serial.println(PID_d);
+//  Serial.println("PID I");
+//  Serial.println(PID_i);
   delay(1500);
+//  if (temp1>30){
+//     digitalWrite(10,HIGH);
+//  digitalWrite(9,LOW);
+//  }
+//  else{
+//  digitalWrite(10,LOW);
+//  digitalWrite(9,HIGH);
+//  }
 }
 
